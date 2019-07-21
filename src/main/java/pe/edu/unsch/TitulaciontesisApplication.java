a218004fab63755f20bd5be1e7cb749dd12e28e7
bonyceron <bony.ceron.27@unsch.edu.pe>
2019-07-20 23:52:17 -0500

    resubiendo template

---
+++ src/main/java/pe/edu/unsch/TitulaciontesisApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.unsch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TitulaciontesisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TitulaciontesisApplication.class, args);
+	}
+
+}
---
+++ src/main/java/pe/edu/unsch/controller/LoginController.java
@@ -0,0 +1,46 @@
+package pe.edu.unsch.controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import pe.edu.unsch.entities.Usuario;
+import pe.edu.unsch.service.UsuarioService;
+
+@Controller
+public class LoginController {
+	
+	@Autowired
+	private UsuarioService usuarioService;
+
+	@GetMapping({"/", "/login"})
+	public String login(Model model) {
+		model.addAttribute("title", "Login | Evaluación 360°");
+		return "views/login/index";
+	}
+
+	@PostMapping("/login")
+	public String login(HttpServletRequest request, HttpSession session, Model model) {
+		model.addAttribute("user", new Usuario());
+		System.out.println("usuario ---- ");
+		System.out.println(request.getParameter("username"));
+		System.out.println(request.getParameter("password"));
+		
+		Usuario user = this.usuarioService.login(request.getParameter("username"), request.getParameter("password"));
+		System.out.println("ester usuario");
+		System.out.println(user);
+		if (user == null) {
+			model.addAttribute("error", "Cuenta inválida");
+			return "redirect:/login";
+		} else {
+			session.setAttribute("usuario", user.getUsuario());
+			return "redirect:/admin/home";
+		}
+	}
+	
+}
